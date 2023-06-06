898a1b5792be0dd00ae9ba9faf0d68005a1afa6b
jinubb <53621550+jinubb@users.noreply.github.com>
2023-06-07 00:26:29 +0900

    init spring-boot project

---
+++ src/main/java/com/webflux/api/WebclientSpringWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.webflux.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclientSpringWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebclientSpringWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webflux/api/WebclientSpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflux.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclientSpringWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
