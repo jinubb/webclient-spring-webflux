bae509fe6d01e8bc1fc2db37601b027448f45bdd
jinubb <53621550+jinubb@users.noreply.github.com>
2023-06-21 11:35:19 +0900

    doc : API swagger 문서 추가

--- src/main/java/com/webflux/api/government/GovernmentRouter.java
+++ src/main/java/com/webflux/api/government/GovernmentRouter.java
@@ -1,7 +1,18 @@
 package com.webflux.api.government;
 
+import com.webflux.api.government.dto.response.SubwayArrivalDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -10,6 +21,25 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 public class GovernmentRouter {
 
     @Bean
+    @RouterOperations(
+            {
+                    @RouterOperation(
+                            path = "/government/subway/arrival",
+                            produces = {MediaType.APPLICATION_JSON_VALUE},
+                            method = RequestMethod.GET,
+                            beanClass = GovernmentHandler.class,
+                            beanMethod = "getSubwayArrival",
+                            operation = @Operation(operationId = "getSubwayArrival",
+                                    parameters = {
+                                            @Parameter(in = ParameterIn.QUERY, name = "text", example = "신사", required = true),
+                                    },
+                                    responses = {
+                                            @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = SubwayArrivalDto.class)))
+                                    }
+                            )
+                    )
+            }
+    )
     public RouterFunction<ServerResponse> routeProductApi(final GovernmentHandler governmentHandler) {
         return RouterFunctions.route()
                 .GET("/government/subway/arrival", governmentHandler::getSubwayArrival)
